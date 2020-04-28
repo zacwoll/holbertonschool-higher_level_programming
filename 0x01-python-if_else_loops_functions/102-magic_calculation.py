04a20633be3c8a86f7186b96013c0eb43ee98aeb
zacwoll <zacwoll@gmail.com>
2020-04-28 19:03:29 +0000

    whatever i didnt commit last

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -4,5 +4,5 @@ def magic_calculation(a, b, c):
         return c
     elif c > b:
         return a + b
-    else
+    else:
         return a * b - c
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(ord('a'), ord('z') + 1):
-    if letter != ord('q') and letter != ord('e'):    
+    if letter != ord('q') and letter != ord('e'):
         print('{:c}'.format(letter), end='')

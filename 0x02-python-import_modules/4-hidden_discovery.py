8f56abf14683727066980f4e2afa9925493d9de6
zacwoll <zacwoll@gmail.com>
2020-04-30 19:28:14 +0000

    removed ) artifact from line 7

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,7 @@ import hidden_4
 
 def main():
     for i in dir(hidden_4):
-        if i[0] != '_' and i[1] != '_'):
+        if i[0] != '_' and i[1] != '_':
             print(i)
 
 if __name__ == "__main__":

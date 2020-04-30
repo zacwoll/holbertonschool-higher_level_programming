a9140d91781bdfd8a149e93c9fe4ae4f8fe625a4
zacwoll <zacwoll@gmail.com>
2020-04-30 19:17:58 +0000

    encapsulated fnc in main()

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 from sys import argv
-argc = len(argv)
-if argc > 1:
-    print("{} argument".format(argc - 1), end='')
-    if argc > 2:
-        print("s", end='')
-    print(":")
-    for i, val in enumerate(argv[1:], 1):
-        print("{}: {}".format(i, val))
-else:
-    print("0 arguments.")
+
+
+def main():
+    argc = len(argv)
+    if argc > 1:
+        print("{} argument".format(argc - 1), end='')
+        if argc > 2:
+            print("s", end='')
+        print(":")
+        for i, val in enumerate(argv[1:], 1):
+            print("{}: {}".format(i, val))
+    else:
+        print("0 arguments.")
+if __name__ == "__main__":
+    main()

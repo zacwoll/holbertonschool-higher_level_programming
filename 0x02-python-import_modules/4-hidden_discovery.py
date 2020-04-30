9c2cf992c3994e73ca097365b6e3dfd4addb4614
zacwoll <zacwoll@gmail.com>
2020-04-30 19:26:31 +0000

    now also check if i[1] != '_'

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,8 +4,8 @@ import hidden_4
 
 def main():
     for i in dir(hidden_4):
-        if i[0] != '_':
+        if i[0] != '_' and i[1] != '_'):
             print(i)
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()

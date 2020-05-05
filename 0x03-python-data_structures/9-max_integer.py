4de78e173cf2406d1de160d8af777c3fbb104999
zacwoll <zacwoll@gmail.com>
2020-05-05 19:54:40 +0000

    covered the empty list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = my_list[0]
-    for x in my_list:
-        if x > max:
-            max = x
-    return max
+    if my_list:
+        max = my_list[0]
+        for x in my_list:
+            if x > max:
+                max = x
+        return max
+    else:
+        return None

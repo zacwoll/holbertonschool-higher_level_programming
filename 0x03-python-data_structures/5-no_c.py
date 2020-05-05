2bf343ec3535208b2e71fae8371bf8af8db6db8e
zacwoll <zacwoll@gmail.com>
2020-05-05 19:44:15 +0000

    it was working the whole time

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    ret = ""
-    for i in range(len(my_string)):
-        if (my_string[i] != 'c' and my_string[i] != 'C'):
-            ret += my_string[i]
+    ret_str = ""
+    for x in my_string:
+        if x != 'c' and x != 'C':
+            ret_str += x
     return ret

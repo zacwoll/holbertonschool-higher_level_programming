5f7e62d4f0c7fcfd26f3598100baaa46347cfb41
zacwoll <zacwoll@gmail.com>
2020-05-05 19:47:00 +0000

    covered empty input check

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not any(matrix):
+        print()
     for x in matrix:
         for idx, y in enumerate(x):
             print("{:d}".format(y), end='\n' if idx is len(x) - 1 else ' ')
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()

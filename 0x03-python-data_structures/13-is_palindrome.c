7a66bb0a6c1230cac1b36c3eab7a39fd1f5b6d3d
zacwoll <zacwoll@gmail.com>
2020-05-05 22:32:39 +0000

    removed a and b bc of lost checks

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -30,17 +30,15 @@ listint_t *reverse_list(listint_t **head)
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head, *fast = *head, *a, *b;
+	listint_t *slow = *head, *fast = *head;
 	
 	while (fast && fast->next)
 	{
 		fast = fast->next->next;
-		a = slow;
 		slow = slow->next;
 	}
 
 	slow = reverse_list(&slow);
-	b = slow;
 	fast = *head;
 	while (fast && slow)
 	{
@@ -49,7 +47,5 @@ int is_palindrome(listint_t **head)
 		fast = fast->next;
 		slow = slow->next;
 	}
-	slow = reverse_list(&b);
-	a->next = slow;
 	return (1);
 }

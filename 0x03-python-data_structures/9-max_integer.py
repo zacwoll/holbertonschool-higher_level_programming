3b58bc2539747eb55845b9e15933b86b80e04132
zacwoll <zacwoll@gmail.com>
2020-05-05 19:51:27 +0000

    removed test lines

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,3 @@ def max_integer(my_list=[]):
         if x > max:
             max = x
     return max
-
-my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))

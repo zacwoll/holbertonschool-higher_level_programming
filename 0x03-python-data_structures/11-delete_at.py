164e9042873ae2a5a09f5a28a46d9eeaa00fa73b
zacwoll <zacwoll@gmail.com>
2020-05-05 19:56:43 +0000

    removed test lines

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,8 +4,3 @@ def delete_at(my_list=[], idx=0):
         return my_list
     del my_list[idx]
     return my_list[:]
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)

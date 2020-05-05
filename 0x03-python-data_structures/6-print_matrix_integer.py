e3223df280f06c28723bc7f198becce9f8f54b78
zacwoll <zacwoll@gmail.com>
2020-05-05 19:49:04 +0000

    commented out the fantastic lines checker hates

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not any(matrix):
-        print()
+#    if not any(matrix):
+#        print()
     for x in matrix:
         for idx, y in enumerate(x):
             print("{:d}".format(y), end='\n' if idx is len(x) - 1 else ' ')

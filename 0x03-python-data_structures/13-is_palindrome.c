4d42d2aa374a2c5558cba3b5b925cd9554771602
zacwoll <zacwoll@gmail.com>
2020-05-05 20:30:51 +0000

    switched approach, signifantly more efficient, hope to get more checks

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,38 +3,49 @@
 #include <stdio.h>
 
 /**
+ * reverse_list - Reverse a linked list
+ * @head: The list
+ * Return: ptr to new head
+ */
+listint_t *reverse_list(listint_t **head)
+{
+	listint_t *prev = NULL, *next;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
  * is_palindrome - determines if singly linked list is palindrome
  * @head: head of list
  * Return: 1 if palindrome, 0 if not
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current;
-	unsigned int n = 0, i, j;
-	int *arr;
+	listint_t *slow = *head, *fast = *head;
+	
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+	}
 
-	current = *head;
-	if (!current)
-		return (1);
-	while (current)
+	slow = reverse_list(&slow);
+	fast = *head;
+	while (fast && slow)
 	{
-		n++;
-		current = current->next;
-	}
-	arr = malloc(sizeof(int) * n);
-	n = 0;
-	current = *head;
-	while (current)
-	{
-		arr[n] = current->n;
-		current = current->next;
-		n++;
-	}
-	for (i = 0, j = n - 1; i < j; i++, j--)
-	{
-		printf("%d : %d\n", arr[i], arr[j]);
-		if (arr[i] != arr[j])
+		if (fast->n != slow->n)
 			return (0);
+		fast = fast->next;
+		slow = slow->next;
 	}
 	return (1);
 }

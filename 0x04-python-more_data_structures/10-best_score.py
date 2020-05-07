dc213769fce13b5e418b21430f10b2e0e4f95cb1
zacwoll <zacwoll@gmail.com>
2020-05-07 20:52:14 +0000

    replaced lambda with .get

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,4 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    return max(a_dictionary, key=lambda key: a_dictionary[key])
+    return max(a_dictionary, key=a_dictionary.get)

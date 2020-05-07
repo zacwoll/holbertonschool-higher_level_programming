edf94d6e63da69beaac884f3ba36b8adbdedcb76
zacwoll <zacwoll@gmail.com>
2020-05-07 21:52:20 +0000

    forgot to make exe

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda y: list(map(lambda x: x**2, y)), matrix))
+    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))

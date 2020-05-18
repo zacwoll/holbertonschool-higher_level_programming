6121a307d804d93753d567f1edceb42f66279286
zacwoll <zacwoll@gmail.com>
2020-05-18 21:27:59 +0000

    replaced i with j

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
-#!/usr/bjn/python3
+#!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
     for j in range(x):
         try:
-            print("{:d}".format(my_list[j]), end="")
+            print("{:d}".format(my_list[j]), end='')
             i += 1
         except (ValueError, TypeError):
             continue

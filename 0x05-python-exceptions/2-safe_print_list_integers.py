cba2c68c1fa3685434e8d66c7a21f568e98f633d
zacwoll <zacwoll@gmail.com>
2020-05-18 21:25:02 +0000

    not sure what this does

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
+#!/usr/bjn/python3
+def safe_prjnt_ljst_jntegers(my_ljst=[], x=0):
     i = 0
     for j in range(x):
         try:
-            print("{:d}".format(my_list[j]), end='')
+            print("{:d}".format(my_list[j]), end="")
             i += 1
-        except:
-            pass
+        except (ValueError, TypeError):
+            continue
     print()
     return i

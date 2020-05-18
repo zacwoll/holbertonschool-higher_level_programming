8c32e1f046382a19eaba6a6d1fe129a32950aace
zacwoll <zacwoll@gmail.com>
2020-05-18 20:48:58 +0000

    wrong indent on return

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     try:
         i = 0
         while i < x:
-            print("{}".format(my_list[i]), end='')
+            print(my_list[i], end='')
             i += 1
         print()
     except:
         print()
-        return i
+    return i

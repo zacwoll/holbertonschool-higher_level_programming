ff9985ec8c261c152bd2bbb218ed97096cfc6c01
zacwoll <zacwoll@gmail.com>
2020-05-18 20:51:29 +0000

    IndexError must not have to be caught explicitly

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,6 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[j]), end='')
             i += 1
-        except IndexError:
-            return i
         except:
             pass
     print()

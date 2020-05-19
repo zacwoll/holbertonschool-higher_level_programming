bf30b78debae9658185b05e7a023c84887a522f2
zacwoll <zacwoll@gmail.com>
2020-05-19 23:41:11 +0000

    checker is nonsense

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,45 +1,45 @@
 #!/usr/bin/python3
-"""Moves a print of a Square Class based on position"""
+"""Define a square with position coordinates."""
 
 
 class Square:
-    """A Square"""
+    """Represents a square."""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Init"""
-        self.__size = size
-        self.__position = position
+        """Initializes the data."""
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """gets the value of size"""
+        """Get the size."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """sets the value of size"""
+        """Set the size."""
         if type(value) != int:
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         self.__size = value
-
-    def area(self):
-        """Area of Square"""
-        return self.__size ** 2
 
     @property
     def position(self):
-        """gets the value of position"""
+        """Get the position."""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Sets the value of position"""
+        """Set the position."""
         if type(value) != tuple or len(value) != 2 or type(value[0]) != int\
-                or type(value[1]) != int or value[0] < 0 or value[1] < 0:
+           or type(value[1]) != int or value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
-            self.__position = value
+        self.__position = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size**2
 
     def my_print(self):
         """Print the square."""

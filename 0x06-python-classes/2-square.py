02d2f2f737880944fc68b409c284ae1836ac972b
zacwoll <zacwoll@gmail.com>
2020-05-19 23:09:16 +0000

    fits pep8 standard

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,6 +6,6 @@ class Square:
         """Init"""
         if type(size) != int:
             raise TypeError("size must be an integer")
-        if size <0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size

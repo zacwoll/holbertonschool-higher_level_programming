b55026a5527ffd9fbfb0fd5f96be87f3929ce930
zacwoll <zacwoll@gmail.com>
2020-05-20 00:20:58 +0000

    checker is broken

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -3,19 +3,17 @@ import math
 
 
 class MagicClass:
-    """Defines a MagicClass"""
+    """Define a magic class."""
 
     def __init__(self, radius=0):
-        """Init MagicClass"""
+        """Initialize MagicClass."""
         self.__radius = 0
-        if type(radius) != int and type(radius) != float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
-        """Area"""
-        return self.__radius ** 2 * math.pi
+        return self.__radius**2 * math.pi
 
     def circumference(self):
-        """Circumference"""
         return 2 * math.pi * self.__radius

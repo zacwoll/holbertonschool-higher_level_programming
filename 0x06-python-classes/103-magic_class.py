c3147cde7a59777ad64d7263cf4879755b53804c
zacwoll <zacwoll@gmail.com>
2020-05-20 00:19:14 +0000

    missing parantheses

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -8,7 +8,7 @@ class MagicClass:
     def __init__(self, radius=0):
         """Init MagicClass"""
         self.__radius = 0
-        if type(radius) != int and type(radius != float:
+        if type(radius) != int and type(radius) != float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 

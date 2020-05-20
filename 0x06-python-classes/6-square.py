2f71597bf32a44151f5ae4f90e387f0cbb43cd4d
zacwoll <zacwoll@gmail.com>
2020-05-20 00:00:03 +0000

    position(self, value) setter was 1 tab off

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,7 +35,7 @@ class Square:
         if type(value) != tuple or len(value) != 2 or type(value[0]) != int\
            or type(value[1]) != int or value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
-            self.__position = value
+        self.__position = value
 
     def area(self):
         """Area of Square"""

19f3e89aaf721dbc073140da497f3e0f6040ec13
zacwoll <zacwoll@gmail.com>
2020-05-20 00:22:13 +0000

    checker is definitely broken

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -13,7 +13,9 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
+        """Area"""
         return self.__radius**2 * math.pi
 
     def circumference(self):
+        """Circumference"""
         return 2 * math.pi * self.__radius

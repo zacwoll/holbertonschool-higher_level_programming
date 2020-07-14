79e9cc0f47c49c2d4a9460bd61aa52b1c19bd015
zacwoll@github.com <zacwoll@github.com>
2020-07-14 22:52:28 +0000

    .

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- shows and genre!
 -- select show.title, genre.name from joined tables
-SELECT tv_shows.title, tv_genres.name FROM tv_shows
+SELECT title, name FROM tv_shows
 JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY title, name;

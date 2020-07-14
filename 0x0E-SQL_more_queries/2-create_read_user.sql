4078dccb79f1b7161c0e4f9c0b4c44542813d394
zacwoll@github.com <zacwoll@github.com>
2020-07-14 19:42:31 +0000

    A

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,7 @@
 -- Read-only user
 -- create db
-CREATE DATABASE IF NOT EXISTS htbn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- create user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON htbn_0d_2.* TO 'user_0d_2'@'localhost';
+-- grant privs
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';

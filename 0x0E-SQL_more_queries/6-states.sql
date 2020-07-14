3b7522fb33aeb7bb2163310ed6406dff2c442f05
zacwoll@github.com <zacwoll@github.com>
2020-07-14 19:44:23 +0000

    added comment?

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- Write a script that creates the database hbtn_0d_usa and the table states
--- (in the database hbtn_0d_usa) on your MySQL server.
+-- create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- create table
 CREATE TABLE IF NOT EXISTS htbn_0d_usa.states (id INT UNIQUE AUTO_INCREMENT NOT NULLL PRIMARY KEY,
 name VARCHAR(256) NOT NULL);

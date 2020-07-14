84a82621d9306723ccc1943414d1d93add3cde12
zacwoll@github.com <zacwoll@github.com>
2020-07-14 22:49:38 +0000

    .

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,6 @@
--- This script lists all movies and their genre
-SELECT title, name FROM tv_show_genres
-JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+-- shows and genre!
+-- select show.title, genre.name from joined tables
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY title, name;

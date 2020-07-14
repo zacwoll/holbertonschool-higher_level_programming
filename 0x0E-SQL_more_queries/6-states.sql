b63c58274a9758abfed0c219a0f849c282484081
zacwoll@github.com <zacwoll@github.com>
2020-07-14 19:45:00 +0000

    idk

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,8 @@
--- Write a script that creates the database hbtn_0d_usa and the table states
--- create database
+-- States table
+-- create db
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- create table
-CREATE TABLE IF NOT EXISTS htbn_0d_usa.states (id INT UNIQUE AUTO_INCREMENT NOT NULLL PRIMARY KEY,
-name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);

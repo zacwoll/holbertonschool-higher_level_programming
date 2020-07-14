5b354b2047ff619af58ade02af18bb80afb126aa
zacwoll@github.com <zacwoll@github.com>
2020-07-14 22:31:39 +0000

    lol actually completed 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,1 +1,5 @@
-Sql
+-- This script lists tv shows and their genres
+SELECT title, tv_show_genres.genre_id FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY title, genre_id;

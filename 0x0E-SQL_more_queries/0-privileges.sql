60b0ddc53a3e4b116879b6f11356c54109c4cbaa
zacwoll@github.com <zacwoll@github.com>
2020-07-14 19:40:44 +0000

    just gonna keep this up

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
--- My privileges
--- show privs for user1
+-- This script lists the privileges for two users
+-- Prints the privileges for user_0d_1
 SHOW GRANTS FOR user_0d_1@localhost;
--- show privs for user2
 SHOW GRANTS FOR user_0d_2@localhost;

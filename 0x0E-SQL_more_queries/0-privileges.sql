267bf4833d9689b0cd6269b6dbef1faf92041fd7
zacwoll@github.com <zacwoll@github.com>
2020-07-14 19:38:43 +0000

    FOR

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
--- 0-privileges.sql
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2 
-SHOW GRANTS user_0d_1@localhost;
-SHOW GRANTS user_0d_2@localhost;
+-- My privileges
+-- show privs for user1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- show privs for user2
+SHOW GRANTS FOR user_0d_2@localhost;

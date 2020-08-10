08a984c5d681a2dc5c04deaa093b436a28e6f3ef
zacwoll <zacwoll@gmail.com>
2020-08-10 04:27:23 +0000

    covered case insensitive

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name LIKE \"n%\" ORDER BY \
-                states.id ASC")
+    cur.execute("SELECT * FROM states WHERE states.name COLLATE \
+                UTF_GENERAL_CI LIKE \"n%\" ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)

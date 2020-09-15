077a323528a9be7cd7a319266144cad249afc543
zacwoll <zacwoll@gmail.com>
2020-09-15 19:40:54 +0000

    semistandard'd 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.argv.length < 4) {
-	console.log(0);
+  console.log(0);
 } else {
-	// 
-	console.log(process.argv.slice(2).sort((a,b) => b - a)[1]);
+  //
+  console.log(process.argv.slice(2).sort((a, b) => b - a)[1]);
 }

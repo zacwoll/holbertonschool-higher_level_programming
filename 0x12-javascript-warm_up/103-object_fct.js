d43469b5326f41d0edae24f7c70ee1644af857d2
zacwoll <zacwoll@gmail.com>
2020-09-15 20:27:46 +0000

    semistandard

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -5,8 +5,8 @@ const myObject = {
 };
 console.log(myObject);
 myObject.incr = function () {
-	myObject.value++;
-}
+  myObject.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
